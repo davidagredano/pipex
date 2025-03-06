928c208500ce292610c31401f3e4a86d0fe0fbba
David Agredano <davidagredano@gmail.com>
2025-03-06 15:49:07 +0100

    Fix: Handle infile open error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 11:53:33 by dagredan          #+#    #+#             */
-/*   Updated: 2025/03/06 14:36:00 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/03/06 15:48:19 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,27 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (argc != 5)
 		return (EXIT_FAILURE);
-	if (access(argv[1], R_OK) == -1)
-	{
-		perror(argv[1]);
-		return (EXIT_FAILURE);
-	}
 	pipe(pipefd);
 	if (fork() == 0)
 	{
 		cmd = cmd_process(argv[2], envp);
+		// Close read end of pipe not used by this subprocess
 		close(pipefd[0]);
+		// Try to open infile
 		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			// If infile open fail, set infile to be /dev/null
+			perror(argv[1]);
+			fd = open("/dev/null", O_RDONLY);
+		}
+		// Set the opened file to be the stdin
 		dup2(fd, STDIN_FILENO);
 		close(fd);
+		// Set the write end of the pipe to be the stdout
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
+		// Execute the command
 		execve(cmd->filename, cmd->argv, cmd->envp);
 	}
 	close(pipefd[1]);
