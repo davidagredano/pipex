0b813ad764a6ed2f0b0c57dc2a5836c44ce40a17
David Agredano <davidagredano@gmail.com>
2025-03-20 20:30:48 +0100

    Fix: Normalize command not found message with bash

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 17:12:33 by dagredan          #+#    #+#             */
-/*   Updated: 2025/03/18 21:39:25 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/03/20 20:28:19 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,8 @@ void	child_cleanup_exit(t_pipex *data, char *error_message, int exit_status)
 {
 	if (exit_status == EXIT_COMMAND_NOT_FOUND)
 	{
-		ft_putstr_fd("command not found: ", STDERR_FILENO);
-		ft_putendl_fd(error_message, STDERR_FILENO);
+		ft_putstr_fd(error_message, STDERR_FILENO);
+		ft_putstr_fd(": command not found\n", STDERR_FILENO);
 	}
 	else if (exit_status == EXIT_PERMISSION_DENIED)
 	{
--- srcs_bonus/utils_bonus.c
+++ srcs_bonus/utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 17:12:33 by dagredan          #+#    #+#             */
-/*   Updated: 2025/03/18 22:22:58 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/03/20 20:28:36 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,8 @@ void	child_cleanup_exit(t_pipex *data, char *error_message, int exit_status)
 {
 	if (exit_status == EXIT_COMMAND_NOT_FOUND)
 	{
-		ft_putstr_fd("command not found: ", STDERR_FILENO);
-		ft_putendl_fd(error_message, STDERR_FILENO);
+		ft_putstr_fd(error_message, STDERR_FILENO);
+		ft_putstr_fd(": command not found\n", STDERR_FILENO);
 	}
 	else if (exit_status == EXIT_PERMISSION_DENIED)
 	{
